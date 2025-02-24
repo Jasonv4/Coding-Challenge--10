5c4c607e7434460b546236f2964d6d0111c1e0c0
Jason Velasquez-Lopez <jasonv11@usf.edu>
2025-02-23 23:14:21 -0500

    Task 2- Created Order Class

--- cc_10.js
+++ cc_10.js
@@ -21,3 +21,21 @@ console.log(prod1.getDetails());
 prod1.updateStock(3);
 console.log(prod1.getDetails()); 
 // Expected output: "Product: Laptop, ID: 101, Price: $1200, Stock: 7
+
+
+//Task 2 Creating an Order Class
+class order { constructor(orderId, product, quantity) {
+    this.orderId = orderId;
+    this.product = product;
+    this.quantity = quantity; 
+    this.product.updateStock(quantity)
+
+} // Creating a class order with properties
+getOrderDetails() {return `Order ID: ${this.orderId}, Product${this.product}, Quantity: ${this.quantity}, Total Price: $${this.product.price * this.quantity}`}
+} // Shows order details
+const order1 = new order(501, prod1, 2);
+console.log(order1.getOrderDetails()); 
+// Expected output: "Order ID: 501, Product: Laptop, Quantity: 2, Total Price: $2400"
+
+console.log(prod1.getDetails()); 
+// Expected output: "Product: Laptop, ID: 101, Price: $1200, Stock: 5" (Stock reduced)
